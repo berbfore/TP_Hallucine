872ec93a205a2f5a575f450a920a305dc072a3d4
Bernet <ber.b.fore@gmail.com>
2022-08-17 15:03:53 -0400

    fetchAll ok (renvoi un tableau)

--- www/connexion.php
+++ www/connexion.php
@@ -9,9 +9,15 @@
     $sql = "SELECT * FROM `movies`;";
     $results = $database->query($sql);
     //var_dump($results->fetch(PDO::FETCH_NUM)); une alternative
-    while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
-        echo $row["title"] . " " . $row["release_date"] . "<br>";
-    }
+    //while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+     //   echo $row["title"] . " " . $row["release_date"] . "<br>";
+   // }
+    
+   //fetchAll
+   $rows = $results->fetchAll(PDO::FETCH_ASSOC);
+   foreach ($rows as $key => $value) {
+    echo $value["title"] . " " . $row["release_date"] . "<br>";
+   }
 
     function connect($host, $dbname, $login, $password){
         return new PDO("mysql:host=".$host.";dbname=".$dbname, $login, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
